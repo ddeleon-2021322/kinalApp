367490de9e935cb393e3d61fb89787fd98f5dd8e
ddeleon-2021322 <ddeleon-2021322@kinal.edu.gt>
2026-05-01 14:48:42 -0600

    Refactor: Se ha creado metodo para llevar al register

--- src/main/java/com/deleondiego/kinalapp/controller/WebController.java
+++ src/main/java/com/deleondiego/kinalapp/controller/WebController.java
@@ -1,11 +1,19 @@
 package com.deleondiego.kinalapp.controller;
 
+import com.deleondiego.kinalapp.entity.Usuario;
+import com.deleondiego.kinalapp.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping("/menu")
     public String home(Model model){
@@ -16,6 +24,18 @@ public class WebController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String mostrarRegistro(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registrarUsuario(@ModelAttribute("usuario") Usuario usuario) {
+        usuarioService.guardar(usuario);
+        return "redirect:/login?success";
     }
 }
 
